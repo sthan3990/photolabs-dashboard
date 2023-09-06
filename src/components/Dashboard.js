3526f8720a3c8144e5648507b8a3f20f0d5130cd
phankons <stephen.han@gmx.com>
2023-09-06 11:02:03 -0400

    Complete section Binding This

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,68 @@
 import React, { Component } from "react";
-
+import Panel from "./Panel";
 import classnames from "classnames";
+import Loading from "./Loading";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza",
+  }
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+  state = {
+    loading: false,
+    focused: null,
+  };
+
+  selectPanel = id => {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null: id
+    }));
+  };
+
+  render() {
+
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
+
 
 export default Dashboard;
